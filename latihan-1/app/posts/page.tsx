3c7dc06b0505a4272bda1151f4654955b8e0e430
Dhony Abu Muhammad <76083017+4bumuhammad@users.noreply.github.com>
2024-03-26 13:18:25 +0700

    updates

--- latihan-1/app/posts/page.tsx
+++ latihan-1/app/posts/page.tsx
@@ -5,8 +5,8 @@ const Posts = () => {
     <>
       <h1 className="text-fuchsia-500">POSTINGAN PAGE</h1>
       <CardList>
-        <p>Hello</p>
-        <i>Hi!</i>
+        <p>Hello</p><br />
+        <i>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</i>
       </CardList>
     </>
   );
